a271bdaf4fd65739e12ddfb4efc65587b8fb05ab
David Holcer <davidholcer@gmail.com>
2025-07-29 17:28:55 -0400

    blog posts with inline code working

--- react-app/.next/static/chunks/webpack.js
+++ react-app/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bc643457ee36305"; }
+/******/ 		__webpack_require__.h = function() { return "d7db1959bb4e469b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
